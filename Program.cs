2c93992aa661b56a9f1ec6cd56f50a04d5208345
Sajeed Shaik <sajeed.shaik@redbus.com>
2020-06-29 08:57:29 +0530

    adding Product class file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PromotionEngine
+{
+    class Program
+    {
+        protected readonly Dictionary<string,int> priceMap = new Dictionary<string,int>{{"A",50},{"B",30},{"C",20},{"D",15}};
+        static void Main(string[] args)
+        {
+            List<string> cart = new List<string>();
+            cart.Add("A");
+            cart.Add("A");
+            cart.Add("A");
+            cart.Add("B");
+            cart.Add("B");
+            cart.Add("B");
+            cart.Add("B");
+            cart.Add("B");
+            cart.Add("C");
+            cart.Add("D");
+
+            
+        }
+    }
+}
